71c8e091a07f0a7d4c4cc9746af5ccb8cfbc6388
Adam Matthews <adamdmatthews@users.noreply.github.com>
2021-06-05 21:03:21 +0100

    created a simple bevy application

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 #![warn(clippy::all, clippy::pedantic)]
 
+use bevy::log::LogPlugin;
+use bevy::prelude::*;
+
+fn hello_world() {
+    info!("Hello, world!");
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::build()
+        .add_plugin(LogPlugin)
+        .add_system(hello_world.system())
+        .run();
 }
