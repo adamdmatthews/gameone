525424bce191fd060e6d2195e2ffb30c569766fa
Adam Matthews <adamdmatthews@users.noreply.github.com>
2021-06-06 13:25:10 +0100

    implemented some very basic camera controls

--- src/game/gameone.rs
+++ src/game/gameone.rs
@@ -1,4 +1,5 @@
 use super::config::Config;
+use super::player::Player;
 use super::world::World;
 
 use bevy::prelude::*;
@@ -6,6 +7,6 @@ use bevy::prelude::*;
 pub struct Gameone;
 impl Plugin for Gameone {
     fn build(&self, app: &mut AppBuilder) {
-        app.add_plugin(Config).add_plugin(World);
+        app.add_plugin(Config).add_plugin(Player).add_plugin(World);
     }
 }
